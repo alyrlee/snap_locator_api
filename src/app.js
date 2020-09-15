38c383b91122ae9b30888ea33520149781df11a1
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-09-15 14:38:43 -0400

    cors issues

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating,formatted_phone_number&key=AIzaSyDPpPhiwe2nBilWB_ihli85BlyRID4DnpU", "localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin","*","https://maps.googleapis.com/maps/api/js?key=AIzaSyDPpPhiwe2nBilWB_ihli85BlyRID4DnpU&libraries=places", "localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
