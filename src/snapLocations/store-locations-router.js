e311b735da77087d838bc17f62bb492d385b5b89
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-10-05 21:22:11 -0400

    store locations

--- src/snapLocations/store-locations-router.js
+++ src/snapLocations/store-locations-router.js
@@ -7,9 +7,6 @@ const {requireAuth} = require('../middleware/jwt-auth');
 const storeLocationsRouter = express.Router();
 const jsonParser = express.json();
 
-const serializeSnapLocationsList = snaplocations => {{
-
-}}
 const userName = 'DemoUser2020'
 const password = 'DemoUserSnap1234!'
 
@@ -24,23 +21,113 @@ fetch(`${API_ENDPOINT}/stores`, {
   })
 })
 
+const serializeSnapLocationsList = store_name => {{
+
+  ObjectId: store_name.ObjectId,
+  store_name: xss(store_name.store_name),
+  address: xss(store_name.address),
+  city: xss(store_name.city),
+  zip5: store_name.zip5,
+}}
+
+storeLocationsRouter
+.route('/')
+.get((req, res, next) => {
+  const knexInstance = req.app.get('db')
+  storeLocationsService.getSnapLocations(knexInstance)
+    .then(store_name => {
+      res.json(store_name.map(serializeSnapLocationsList))
+    })
+    .catch(next)
+})
+
+
+for (const [ObjectId, Store_Name] of Object.entries(newStore))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+
+    storeLocationsService.insertSnapLocations(
+      req.app.get('db'),
+      newStore
+    )
+      .then(store_name => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${store_name.ObjectId}`))
+          .json(serializeSnapLocationsList(store_name))
+      })
+      .catch(next)
+  })
+
+articlesRouter
+  .route('/:store_name_ObjectId')
+  .all((req, res, next) => {
+    storeLocationsService.getByObjectId(
+      req.app.get('db'),
+      req.params.store_name.ObjectId
+    )
+      .then(store_name => {
+        if (!store_name) {
+          return res.status(404).json({
+            error: { message: `Store location doesn't exist` }
+          })
+        }
+        res.store_name = store_name
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeSnapLocationsList(res.store_name))
+  })
+  .delete((req, res, next) => {
+    storeLocationsService.deleteStore_Name(
+      req.app.get('db'),
+      req.params.store_name.ObjectId
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { ObjectId, Store_Name } = req.body
+    const newStore = { ObjectId, Store_Name }
+
+    const numberOfValues = Object.values(new_storeToUpdate).filter(Boolean).length
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must content either 'store name', 'object id'`
+        }
+      })
+
+    storeLocationsService.updateStore_Name(
+      req.app.get('db'),
+      req.params.store_name.ObjectId
+      store_nameToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
 
 .post(jsonParser, (req, res, next) => {
 
     const { ObjectId, Store_Name } = req.body
     const newStore = { ObjectId, Store_Name }
 
-    for (const [key, value] of Object.entries(newStore)) {
+    for (const [ObjectId, Store_Name] of Object.entries(newStore)) {
       if (value == null) {
         return res.status(400).json({
           error: `Missing '${key}' in request body`
         })
       }
     }
-
-    storeLocationsService.insertStoreLocation(
-      req.app.get('db'),
-      newStore
-    )
-    /* some code not shown */
-  })
+  
+    
+ 
+  module.exports = storeLocationsRouter;
