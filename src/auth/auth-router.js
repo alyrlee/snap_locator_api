11d7addb223f96925a527ec84bda340b5bdd3db2
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-02-02 09:48:45 -0500

    auth working for login

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -17,94 +17,34 @@ authRouter
           error: `Missing '${key}' in request body`
         })
 
-        AuthService.getUserWithUserName(
+  AuthService.getUserWithUserName(
           req.app.get('db'),
-          loginUser.user_name
+          loginCreds.user_name
         )
           .then(dbUser => {
             if (!dbUser)
               return res.status(400).json({
-                error: 'Incorrect user_name or password',
+                error: 'Incorrect user_name',
               })
     
-            return AuthService.comparePasswords(loginUser.password, dbUser.password)
-              .then(compareMatch => {
-                if (!compareMatch)
-                  return res.status(400).json({
-                    error: 'Incorrect user_name or password',
-                  })
-    
-                const sub = dbUser.user_name
-                const payload = { user_id: dbUser.id }
-                res.send({
-                  authToken: AuthService.createJwt(sub, payload),
-                })
+            console.log('login match', req.body.password, dbUser.password)
+            if  (req.body.password === dbUser.password) {
+              const sub = dbUser.user_name
+              const payload = { user_id: dbUser.id }
+              res.send({
+                authToken: AuthService.createJwt(sub, payload),
               })
+
+          } else {
+            return res.status(400).json({
+              error: 'Incorrect password',
+            })
+          }
           })
           .catch(next)
       })
     
     module.exports = authRouter
           
-         
-//   AuthService.getUserWithUserName(
-//       req.app.get('db'),
-//       loginCreds.user_name
-//     )  
-//     .then(dbUser => {
-//       if (!dbUser) res.sendStatus(204); 
-//       else { 
-//         bcrypt.compare(req.body.password, dbUser.password) 
-//         .then(passwordMatch => passwordMatch ? 
 
-//    authRouter.post('/login', requireAuth, (req, res) => {
-//         const sub = req.dbUser.user_name
-//         const payload = { user_id: req.dbUser.id }  
-//         res.send({
-//                authToken: AuthService.createJwt(sub, payload),
-//        })
-//       })
-//          res:sendStatus(200)
-//       }
- 
-//       return AuthService.hashPassword(password)
-//       .then(hashedPassword => {
-//         const newUser = {
-//           user_name,
-//           password: hashedPassword,
-//           full_name,
-//           date_created: 'now()',
-//         }
-
-//         return AuthService.insertUser(
-//           req.app.get('db'),
-//           newUser
-//         )
-//           .then(user => {
-//             res
-//               .status(201)
-//               .location(path.posix.join(req.originalUrl, `/${user.id}`))
-//               .json(UsersService.serializeUser(user))
-//           })
-//       })
-//   })
-//   .catch(next)
-// })
-
-
-
-// module.exports = authRouter
-
-
-//var passwordHash = require('./lib/password-hash'); var hashedPassword = 'sha1$3I7HRwy7$cbfdac6008f9cab4083784cbd1874f76618d2a97'; 
-//console.log(passwordHash.verify('password123', hashedPassword)); // true console.log(passwordHash.verify('Password0', hashedPassword)); // false
-// const hash = '';
-// return AuthService.comparePasswords
-// (password,hash)
-//          .then(password => {
-//            if (!password)
-//             return res.status(400).json({
-//                error: 'Incorrect password',
-//             })  
-                                       
   
