83b75b901685815a1c8d890d28606d0c008d64e9
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-02-21 14:06:49 -0500

    prepare migration

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://murmuring-shore-59851.herokuapp.com',
     DATABASE_URL: 'postgresql://ashley_dev:201030410Vera@localhost/snap_app',
     TEST_DATABASE_URL: 'postgresql://ashley_dev:201030410Vera@localhost/snap_app_auth_test',
     JWT_SECRET: process.env.JWT_SECRET || 'snap_app_locator_secret',
