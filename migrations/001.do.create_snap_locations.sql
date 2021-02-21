67c832bb9aa5021703066ce3424d22dedca10fbc
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-02-21 15:54:21 -0500

    prepare to deploy

--- migrations/001.do.create_snap_locations.sql
+++ migrations/001.do.create_snap_locations.sql
@@ -16,13 +16,13 @@ CREATE TABLE snap_locations (
 );
 
 CREATE TABLE snap_app_users (
-  user_id uuid DEFAULT uuid_generate_v4() NOT NULL,
+  user_id INTEGER PRIMARY KEY,
   user_name TEXT NOT NULL UNIQUE,
   full_name TEXT NOT NULL,
   email TEXT NOT NULL, 
   password TEXT NOT NULL,
   nickname TEXT,
-  PRIMARY KEY(user_id),
+  -- PRIMARY KEY(user_id),
   date_created TIMESTAMPTZ NOT NULL DEFAULT now(),
   date_modified TIMESTAMPTZ
 );
