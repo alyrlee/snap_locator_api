1a03930eda768dbef05cfaf571639d989bd3ee77
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-03-11 08:57:04 -0500

    made revisions upon mentor's suggestions

--- src/snapLocations/store-locations-router.js
+++ src/snapLocations/store-locations-router.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const xss = require('xss');
+const express = require("express");
+const xss = require("xss");
 const jsonParser = express.json();
-const path = require('path');
-const storeLocationsService = require('./store-locations-service');
+const path = require("path");
+const storeLocationsService = require("./store-locations-service");
 
 const storeLocationsRouter = express.Router();
 
-const serializeSnapLocationsList = Store_Name => ({
+const serializeSnapLocationsList = (Store_Name) => ({
   ObjectId: Store_Name.ObjectId,
   store_name: xss(Store_Name.Store_Name),
   address: xss(Store_Name.address),
@@ -14,131 +14,157 @@ const serializeSnapLocationsList = Store_Name => ({
   state: xss(Store_Name.state),
   zip5: Store_Name.zip5,
   latitude: Store_Name.latitude,
-  longitude: Store_Name.longitude
-})
+  longitude: Store_Name.longitude,
+});
 
 storeLocationsRouter
-  .route('/')
+  .route("/")
   .get((req, res, next) => {
-    const knexInstance = req.app.get('db')
-    storeLocationsService.getSnapLocations(knexInstance)
-      .then(Store_Name => {
-        res.json(Store_Name.map(serializeSnapLocationsList))
+    const knexInstance = req.app.get("db");
+    storeLocationsService
+      .getSnapLocations(knexInstance)
+      .then((Store_Name) => {
+        res.json(Store_Name.map(serializeSnapLocationsList));
       })
-      .catch(next)
+      .catch(next);
   })
-.post(jsonParser, (req, res, next) => {
-  const { Store_Name } = req.body
-  const newSnapLocation = { Store_Name }
-
-  for (const [key, value] of Object.entries(newSnapLocation)) {
-    if (value == null) {
-      return res.status(400).json({
-        error: `Missing '${key}' in request body`
-      })
-    }
-  }
-})
-
-storeLocationsRouter
-  .route('/cityState')
-  .get(jsonParser, (req, res, next) => {
-    console.log("request is: ", req);
-    const { city, state } = req.body;
-    console.log('we received city/state from front end??', city, state) 
-    const knexInstance = req.app.get('db')
-    storeLocationsService.getSnapCityState(knexInstance, city, state)
-        .then(city, state => {
-          // this is how you organize your response back to the front end
-          console.log('~~~~this is the format will be sending back!\n~~~~', city, state)
-          res.json({city, state})
-          // res.json(city && state.map(serializeSnapLocationsList))
-      })
-          .catch(next)
-  })
-
   .post(jsonParser, (req, res, next) => {
-    const { city, state } = req.body
-    const newSnapLocation = { city, state }
-  
+    const { Store_Name } = req.body;
+    const newSnapLocation = { Store_Name };
+
     for (const [key, value] of Object.entries(newSnapLocation)) {
       if (value == null) {
         return res.status(400).json({
-          error: `Missing '${key}' in request body`
-        })
+          error: `Missing '${key}' in request body`,
+        });
       }
+    }
+  });
 
-      storeLocationsService.insertSnapLocations(
-        req.app.get('db'),
-            newSnapLocation
-          )
-              .then(Store_Name => {
-                res
-                  .status(201)
-                  .location(path.posix.join(req.originalUrl, `/${Store_Name.ObjectId}`))
-                  .json(serializeSnapLocationsList(Store_Name))
-            })
-            .catch(next)
-        }
+storeLocationsRouter
+  .route("/cityState")
+  .get(jsonParser, (req, res, next) => {
+    console.log("request is: ", req);
+    const { city, state } = req.body;
+    console.log("we received city/state from front end??", city, state);
+    const knexInstance = req.app.get("db");
+    storeLocationsService
+      .getSnapCityState(knexInstance, city, state)
+      .then(city, (state) => {
+        // this is how you organize your response back to the front end
+        console.log(
+          "~~~~this is the format will be sending back!\n~~~~",
+          city,
+          state
+        );
+        res.json({ city, state });
+        // res.json(city && state.map(serializeSnapLocationsList))
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    console.log("request is: ", req);
+    const { city, state } = req.body;
+    console.log("we received city/state from front end??", city, state);
+    const knexInstance = req.app.get("db");
+    storeLocationsService
+      .getSnapCityState(knexInstance, city, state)
+      .then(city, (state) => {
+        // this is how you organize your response back to the front end
+        console.log(
+          "~~~~this is the format will be sending back!\n~~~~",
+          city,
+          state
+        );
+        res.json({ city, state });
+        // res.json(city && state.map(serializeSnapLocationsList))
+      })
+      .catch(next);
+    // const newSnapLocation = { city, state };
 
-storesLocationsRouter
-  .route('/stores')
-  .all(requireAuth)
-  .all((req, res, next) => {
-    storeLocationsService.getStoreName(
-      req.app.get('db'),
-      req.params.Store_Name.Store_Name,
-      req.user.id
-    )
-      .then(Store_Name => {
-        if (!Store_Name) {
-          return res.status(404).json({
-            error: { message: `Store location doesn't exist` }
-          })
-        }
-        res.Store_Name = Store_Name
-        next()
-      })
-      .catch(next)
-  })
-  
-  .get((req, res, next) => {
-    res.json(serializeSnapLocationsList(res.Store_Name))
-  })
-    
-  .delete((req, res, next) => {
-    storeLocationsService.deleteStore_Name(
-      req.app.get('db'),
-      req.params.store_name.ObjectId
-    )
-      .then(numRowsAffected => {
-        res.status(204).end()
-      })
-      .catch(next)
-  })
-  .patch(jsonParser, (req, res, next) => {
-    const { Store_Name } = req.body
-    const new_snapLocationToUpdate = { Store_Name }
+    // for (const [key, value] of Object.entries(newSnapLocation)) {
+    //   if (value == null) {
+    //     return res.status(400).json({
+    //       error: `Missing '${key}' in request body`,
+    //     });
+    //   }
+    // }
+  });
+      
 
-    const numberOfValues = Object.values(new_snapLocationToUpdate).filter(Boolean).length
-    if (numberOfValues === 0)
-      return res.status(400).json({
-        error: {
-          message: `Request body must content either 'SNAP store name'`
-        }
-      })
+  // storeLocationsService
+  //   .insertSnapLocations(req.app.get("db"), newSnapLocation)
+  //   .then((Store_Name) => {
+  //     res
+  //       .status(201)
+  //       .location(
+  //         path.posix.join(req.originalUrl, `/${Store_Name.ObjectId}`)
+  //       )
+  //       .json(serializeSnapLocationsList(Store_Name));
+  //   })
+  //   .catch(next);
 
-    storeLocationsService.updateStore_Name(
-      req.app.get('db'),
-      req.params.Store_Name.Store_Name,
-      store_nameToUpdate
-    )
-      .then(updateStore_Name => {
-        res.status(204).json(serializeSnapLocationsList(updateStore_Name[0]))
-      })
-      .catch(next)
-  })
-})
+  //   storeLocationsRouter
+  //     .route("/stores")
+  //     // .all(requireAuth) //need to reimplement to make it a protected route, optional
+  //     .all((req, res, next) => {
+  //       storeLocationsService
+  //         .getStoreName(
+  //           req.app.get("db"),
+  //           req.params.Store_Name.Store_Name,
+  //           req.user.id
+  //         )
+  //         .then((Store_Name) => {
+  //           if (!Store_Name) {
+  //             return res.status(404).json({
+  //               error: { message: `Store location doesn't exist` },
+  //             });
+  //           }
+  //           res.Store_Name = Store_Name;
+  //           next();
+  //         })
+  //         .catch(next);
+  //     })
+
+  //     .get((req, res, next) => {
+  //       res.json(serializeSnapLocationsList(res.Store_Name));
+  //     })
+
+  //     .delete((req, res, next) => {
+  //       storeLocationsService
+  //         .deleteStore_Name(req.app.get("db"), req.params.store_name.ObjectId)
+  //         .then((numRowsAffected) => {
+  //           res.status(204).end();
+  //         })
+  //         .catch(next);
+  //     })
+  //     .patch(jsonParser, (req, res, next) => {
+  //       const { Store_Name } = req.body;
+  //       const new_snapLocationToUpdate = { Store_Name };
+
+  //       const numberOfValues = Object.values(new_snapLocationToUpdate).filter(
+  //         Boolean
+  //       ).length;
+  //       if (numberOfValues === 0)
+  //         return res.status(400).json({
+  //           error: {
+  //             message: `Request body must content either 'SNAP store name'`,
+  //           },
+  //         });
+  //   });
+
+  //   storeLocationsService
+  //     .updateStore_Name(
+  //       req.app.get("db"),
+  //       req.params.Store_Name.Store_Name,
+  //       store_nameToUpdate
+  //     )
+  //     .then((updateStore_Name) => {
+  //       res
+  //         .status(204)
+  //         .json(serializeSnapLocationsList(updateStore_Name[0]));
+  //     })
+  //     .catch(next);
 
 
 module.exports = storeLocationsRouter;
