f589be8071178ca304562876cedaf448dc085e21
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-03-22 15:50:00 -0400

    prepare for deploy

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin","*","https://murmuring-shore-59851.herokuapp.com/", ); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin","*","https://murmuring-shore-59851.herokuapp.com", ); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
