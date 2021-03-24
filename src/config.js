e01354dde957fee6fc21cd0a82a45e33e620c594
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-03-24 12:12:15 -0400

    perapre to deploy

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     // API_ENDPOINT: 'http://localhost:8000/api',
-    API_ENDPOINT: 'https://murmuring-shore-59851.herokuapp.com/api',
+    API_ENDPOINT: 'https://murmuring-shore-59851.herokuapp.com/api/',
     DATABASE_URL: 'postgres://xjjnbkymmwibzv:2aa5a60dda71fc581ece0493a74901ade2fd024cd9e117db0e36c3afbfc7c420@ec2-52-7-168-69.compute-1.amazonaws.com:5432/d58gi1rjhfmr7g',
     TEST_DATABASE_URL: 'postgres://xjjnbkymmwibzv:2aa5a60dda71fc581ece0493a74901ade2fd024cd9e117db0e36c3afbfc7c420@ec2-52-7-168-69.compute-1.amazonaws.com:5432/d58gi1rjhfmr7g_test',
     // DATABASE_URL: 'postgresql://ashley_dev:201030410Vera@localhost/snap_app',
