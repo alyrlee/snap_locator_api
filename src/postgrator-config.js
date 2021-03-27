eb5c82e0d011fa974caf346d35846f173cb89ca0
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-03-27 12:29:57 -0400

    prepare to deploy

--- src/postgrator-config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-console.log('config info', config());
-module.exports = {
-  "migrationDirectory": "migrations",
-  "driver": "pg",
-  "connectionString": (pg.defaults.sll = process.env.NODE_ENV === "production")
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
-}
