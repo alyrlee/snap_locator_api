2e21d24fab0bde6acd8b32d3891889c6d88dd359
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-03-27 12:08:32 -0400

    prepare to deploy

--- src/postgrator-config.js
+++ src/postgrator-config.js
@@ -4,6 +4,6 @@ module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
   "connectionString": (pg.defaults.sll = process.env.NODE_ENV === "production")
-    ? process.env.TEST_DB_URL
-    : process.env.DB_URL,
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
