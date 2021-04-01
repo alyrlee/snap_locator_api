f71b8827d904de4c4bfbc8cb72027e0d5728eb20
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-03-31 22:37:54 -0400

    updated user saved location table

--- seeds/seed.user_saved_locations.sql
+++ seeds/seed.user_saved_locations.sql
@@ -4,6 +4,9 @@ BEGIN;
 
 INSERT INTO user_saved_locations (id, date_created, ObjectId, user_id, saved_location_category) 
 VALUES
-  ('1','2020-12-15 20:57:43.539035-05','6567','4c4b260f-d786-4b9f-b293-ba523827b848','Favorites');
+  ('1','2020-12-15 20:57:43.539035-05','6567','4c4b260f-d786-4b9f-b293-ba523827b848','Favorites'),
+  ('2','2020-12-15 20:57:43.539035-05','211322','4c4b260f-d786-4b9f-b293-ba523827b848','To Visit'),
+  ('3','2020-12-15 20:57:43.539035-05','211323','4c4b260f-d786-4b9f-b293-ba523827b848','Favorites'),
+  ('4','2020-12-15 20:57:43.539035-05','211324','4c4b260f-d786-4b9f-b293-ba523827b848','Favorites');
 
   COMMIT;
