330702e70feda9e67bff8d86c56bf69df2dcd5bf
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-04-15 14:19:27 -0400

    testing auth-end

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -4,10 +4,16 @@ const app = require("../src/app");
 const helpers = require("./test-helpers");
 
 describe.only("Auth Endpoints", function () {
-  let db
+  let db;
 
-  const { testUsers } = helpers.makeSnapFixtures()
-  const testUser = testUsers[0]
+  const { testUsers } = helpers.makeSnapFixtures();
+  // const testUser = testUsers[0];
+
+  after("disconnect from db",() => db.destroy());
+
+  beforeEach("cleanup",() => db('snap_app_users').truncate());
+
+  afterEach("cleanup",() => db('snap_app_users').truncate());
  
   before("make knex instance", async () => {
     db = knex({
@@ -15,34 +21,42 @@ describe.only("Auth Endpoints", function () {
       connection: process.env.TEST_DATABASE_URL,
     });
     console.log("what is the test db", process.env.TEST_DATABASE_URL);
-    app.set("db", db)
+    app.set("db", db);
     await db.schema.createTable('snap_app_users', (table) => {
         table.integer('id')
-        table.string('user_name')
-        table.integer('password')
+        table.varchar(255('user_name'))
+        table.varchar(255('password'))
       })
-    });
   });
 
-  after("disconnect from db",() => db.destroy())
-
-  beforeEach("cleanup",() => db('snap_app_users').truncate())
-
-  afterEach("cleanup",() => db('snap_app_users').truncate())
-
   describe(`POST /api/auth/login`, () => {
-    beforeEach("insert users", () =>  {
+    beforeEach("insert", () =>  {
       return db
       .into('snap_app_users')
-      .insert(testUsers)
-    })
+      .insert(testUsers);
+    });
     
     const requiredFields = ['user_name', 'password']
 
+  
+    const loginNoUserName = {
+      password: 'DemoUser2020*'
+    };
+  
+    it(`responds with 400 required error when 'user_name' is missing`, () => {
+        // delete loginAttemptBody[field]
+        return supertest(app)
+            .post('/api/auth/login')
+            .send(loginNoUserName)
+            .expect(400, {
+                error: `Missing 'user_name' in request body`,
+            })
+    })
+  
     requiredFields.forEach(field => {
         const loginAttemptBody = 
         { user_name: 'Demo User', password: 'DemoUser2020*' }
-
+  
         it(`responds with 400 required error when '${field}' is missing`, () => {
             // delete loginAttemptBody[field]
             return supertest(app)
@@ -52,8 +66,8 @@ describe.only("Auth Endpoints", function () {
                     error: `Missing '${field}' in request body`,
                 })
         })
-    })
-
+    });
+  
     it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
     const userInvalidUser = { user_name: 'user-not', password: 'existy' }
     return supertest(app)
@@ -61,7 +75,7 @@ describe.only("Auth Endpoints", function () {
         .send(userInvalidUser)
         .expect(400, { error: `Incorrect user_name or password` })
     })
-
+  
     it(`responds 400 'invalid user_name or password' when bad password`, () => {
     const userInvalidPass = { user_name: testUser.user_name, password: 'incorrect' }
     return supertest(app)
@@ -69,7 +83,7 @@ describe.only("Auth Endpoints", function () {
         .send(userInvalidPass)
         .expect(400, { error: `Incorrect user_name or password` })
     })
-
+  
     it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
     const userValidCreds = {
         user_name: testUser.user_name,
@@ -90,5 +104,7 @@ describe.only("Auth Endpoints", function () {
         authToken: expectedToken,
         })
     })
+  });
+
+
 })
-// })
