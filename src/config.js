371e89a94143513b080c0ea7ff95ec9a9b5cf5ae
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-04-19 11:18:42 -0400

    prepare to deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_ENDPOINT: 'https://murmuring-shore-59851.herokuapp.com/api',
+    API_ENDPOINT: 'https://murmuring-shore-59851.herokuapp.com/',
     DATABASE_URL: 'postgres://nsdquldjuminiw:52cdbad046bd1b9bc25ea812be935abd669e8a87c931dac3e6002d65077af50c@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d9hqjm9h9fb7er',
     TEST_DATABASE_URL: 'postgres://nsdquldjuminiw:52cdbad046bd1b9bc25ea812be935abd669e8a87c931dac3e6002d65077af50c@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d9hqjm9h9fb7er_test',
     JWT_SECRET: process.env.JWT_SECRET || 'snap_app_locator_secret',
