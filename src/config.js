19194be30682a3519020e65df89cb984ddcb6b88
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-04-19 21:27:40 -0400

    prepare to deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_ENDPOINT: 'https://murmuring-shore-59851.herokuapp.com/',
+    API_ENDPOINT: 'https://murmuring-shore-59851.herokuapp.com/api',
     DATABASE_URL: process.env.DATABASE_URL,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET || 'snap_app_locator_secret',
