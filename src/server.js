7351e9c576a71fb117dd8229eeee28d1603e7963
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-04-21 12:14:07 -0400

    prepare to depoy

--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@ const {PORT, DATABASE_URL} = require('./config');
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
-  ssl: true,
+  // ssl: true,
+  strictSSL: false,
   rejectUnauthorized: false
 });
 
